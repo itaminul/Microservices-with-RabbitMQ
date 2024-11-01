621c087f1df6483876242590975e2bc95fe2ea4e
itaminul <itaminul@gmail.com>
2024-11-01 22:40:30 +0600

    order producer

--- order-service/src/service/orderService.ts
+++ order-service/src/service/orderService.ts
@@ -2,21 +2,40 @@ import { NextFunction, Request, Response } from "express";
 import { AppDataSource } from "../data-source";
 import { Order } from "../entity/Order";
 import successResponse from "../middlewares/successResponse";
-import { sendOrderMessage } from "../rabbitMQ/orderMessages";
-
+import { OrderProducer } from "../rabbitMQ/orderProducer";
 export class OrderService {
+  async createOrder(req: Request, res: Response, next: NextFunction) {
+    const orders = Array.isArray(req.body) ? req.body : [req.body];
+    //start a transaction
+    const queryRunner = AppDataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const savedOrders = await queryRunner.manager.save(Order, orders);
+      await Promise.all(savedOrders.map((order) => OrderProducer(order)));
+      // Commit the transaction
+      await queryRunner.commitTransaction();
+      successResponse(res, savedOrders);
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      next(error);
+    } finally {
+      await queryRunner.release();
+    }
+  }
+  /*
   async createOrder(req: Request, res: Response, next: NextFunction) {
     try {
       const orderRepository = AppDataSource.getRepository(Order);
       const orders = Array.isArray(req.body) ? req.body : [req.body];
       const savedOrders = await orderRepository.save(orders);
       // await Promise.all(savedOrders.forEach((order) => sendOrderMessage(order)));
-      await Promise.all(savedOrders.map((order) => sendOrderMessage(order)));
+      await Promise.all(savedOrders.map((order) => OrderProducer(order)));
       successResponse(res, savedOrders);
     } catch (error) {
       next(error);
     }
-  }
+  }*/
 
   async getAll(req: Request, res: Response, next: NextFunction) {
     try {
